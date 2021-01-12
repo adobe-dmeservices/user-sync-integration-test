aa812b0c6184fd7578c24374affdf5a2aa1e3561
vossen-adobe <per38285@adobe.com>
2021-01-12 17:20:56 -0600

    reset 2

--- ust_integration_test/tests.py
+++ ust_integration_test/tests.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import re
 import sys
 
 from ust_integration_test import init_logger
@@ -21,22 +22,27 @@ def run_test(test_name, test_dir):
 def delete_users(tmpdir):
     run_test('setup.delete_all_users', tmpdir)
 
+
 def reset_sign_users(tmpdir):
+    logger.info("Reset sign users")
     run_test('setup.reset_sign_users', tmpdir)
 
+
 def test_manual(tmpdir):
-    #delete_users(tmpdir)
+    # delete_users(tmpdir)
     reset_sign_users(tmpdir)
+
 
 class TestUST(object):
 
     def test_create_all_users(self, tmpdir):
         run_test(get_test_name(), tmpdir)
 
-    def test_sign_reset(self, tmpdir):
+    def test_sign_case_1(self, tmpdir):
+        reset_sign_users(tmpdir)
         run_test(get_test_name(), tmpdir)
 
 
 # noinspection PyProtectedMember,PyUnresolvedReferences
 def get_test_name():
-    return sys._getframe(1).f_code.co_name
+    return re.sub('^test_', '', sys._getframe(1).f_code.co_name)
